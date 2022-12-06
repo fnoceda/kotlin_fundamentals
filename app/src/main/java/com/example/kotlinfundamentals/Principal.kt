125732ca933a676b7fa4c988cf104f844daf7725
fnoceda83 <fnoceda83@gmail.com>
2022-12-06 15:19:57 -0300

    class 2

--- app/src/main/java/com/example/kotlinfundamentals/Principal.kt
+++ app/src/main/java/com/example/kotlinfundamentals/Principal.kt
@@ -2,10 +2,24 @@ package com.example.kotlinfundamentals
 
 fun main(){
     println("Hola Kotlin");
-    newTopic("Hola otro lenguaje");
+    newTopic("Variables y Constantes");
+    val a = true;
+    println("a = $a");
+
+    var b = 2;
+    println("b = $b")
+
+    var c: Int;
+    c = 5;
+    println("c = $c")
 }
 
-
 fun newTopic(topic: String ){
-    println(topic);
+    println()
+    print("========== ")
+    print(topic);
+    print(" ==========")
+    println()
+    print("\n========== $topic ==========")
+    println()
 }
