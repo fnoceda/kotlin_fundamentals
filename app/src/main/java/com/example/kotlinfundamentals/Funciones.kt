65e2efc07c171f7e7c5a16276cc6cad6ca897fcd
fnoceda83 <fnoceda83@gmail.com>
2022-12-07 10:33:15 -0300

    clase 5

--- app/src/main/java/com/example/kotlinfundamentals/Funciones.kt
+++ app/src/main/java/com/example/kotlinfundamentals/Funciones.kt
@@ -1,5 +1,7 @@
 package com.example.kotlinfundamentals
 
+import kotlin.math.abs
+
 fun main(){
     sayHello()
     newTopic("Argumentos")
@@ -7,16 +9,12 @@ fun main(){
     var b = 3
     println("$a + $b = ${sum(a,b)}")
     println("$a - $b = ${sub(a,b)}")
-
+    newTopic("Infix")
+    val c = -3
+    println(c.enableAbs(false))
 }
-
-private fun sayHello() {
-    println("Hola Kotlin")
-}
-
-fun sum(a:Int, b:Int): Int{
-
-    return a + b
-}
-
+private fun sayHello() { println("Hola Kotlin") }
+fun sum(a:Int, b:Int): Int{ return a + b }
 fun sub(a:Int, b:Int) = a - b
+
+infix fun Int.enableAbs(enable: Boolean) = if(enable) abs(this) else this
