40ec0f5d0b3890cf2b4b29fd8103c0562deabd03
fnoceda83 <fnoceda83@gmail.com>
2022-12-08 12:14:54 -0300

    Enum, copy

--- app/src/main/java/com/example/kotlinfundamentals/Clases.kt
+++ app/src/main/java/com/example/kotlinfundamentals/Clases.kt
@@ -15,7 +15,14 @@ fun main(){
     println("Es Privado => ${smartphone.isPrivate}")
     */
     newTopic("Data Classes")
-    val myUser = User(0, "Juan",  "Perez",  1)
+    val myUser = User(0, "Juan",  "Perez",  Group.FAMILY, Group.FAMILY.ordinal)
     println(myUser)
     println(myUser.component3())
+
+    val bro = myUser.copy( id =1,  name ="Luis")
+    println(bro)
+
+    val friend = bro.copy( id =1,  name ="Roberto", lastName = "Ruiz", group = Group.FRIENDS)
+    println(friend)
+
 }
--- app/src/main/java/com/example/kotlinfundamentals/User.kt
+++ app/src/main/java/com/example/kotlinfundamentals/User.kt
@@ -1,5 +1,7 @@
 package com.example.kotlinfundamentals
 
-data class User(val id: Long, var name: String, var lastName: String, var group: Int) {
+enum class Group{FAMILY, WORK, FRIENDS }
+
+data class User(val id: Long, var name: String, var lastName: String, var group: Group, var group2: Int) {
 
 }
