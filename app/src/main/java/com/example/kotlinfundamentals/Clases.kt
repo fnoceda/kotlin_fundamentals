4a37b7000bd774dae88fe6fda838b6cb685ea3d2
fnoceda83 <fnoceda83@gmail.com>
2022-12-08 12:04:47 -0300

    Data Class

--- app/src/main/java/com/example/kotlinfundamentals/Clases.kt
+++ app/src/main/java/com/example/kotlinfundamentals/Clases.kt
@@ -1,15 +1,21 @@
 package com.example.kotlinfundamentals
 
 fun main(){
+    /*
     newTopic("Clases")
     val phone: Phone=Phone(123)
     phone.call()
     phone.showNumber()
-//    println(phone.number)
+    //    println(phone.number)
 
     newTopic("Herencia")
     val smartphone = Smartphone(456, true)
     smartphone.call()
     smartphone.showNumber()
     println("Es Privado => ${smartphone.isPrivate}")
+    */
+    newTopic("Data Classes")
+    val myUser = User(0, "Juan",  "Perez",  1)
+    println(myUser)
+    println(myUser.component3())
 }
---
+++ app/src/main/java/com/example/kotlinfundamentals/User.kt
@@ -0,0 +1,5 @@
+package com.example.kotlinfundamentals
+
+data class User(val id: Long, var name: String, var lastName: String, var group: Int) {
+
+}
