617291095feaf767d7614a39c3025534d1332e5b
fnoceda83 <fnoceda83@gmail.com>
2022-12-08 12:24:47 -0300

    funciones de alcance y apply

--- app/src/main/java/com/example/kotlinfundamentals/Clases.kt
+++ app/src/main/java/com/example/kotlinfundamentals/Clases.kt
@@ -7,22 +7,38 @@ fun main(){
     phone.call()
     phone.showNumber()
     //    println(phone.number)
-
+*/
     newTopic("Herencia")
     val smartphone = Smartphone(456, true)
     smartphone.call()
     smartphone.showNumber()
-    println("Es Privado => ${smartphone.isPrivate}")
-    */
+//    println("Es Privado => ${smartphone.isPrivate}")
+
     newTopic("Data Classes")
     val myUser = User(0, "Juan",  "Perez",  Group.FAMILY, Group.FAMILY.ordinal)
-    println(myUser)
-    println(myUser.component3())
+//    println(myUser)
+//    println(myUser.component3())
 
     val bro = myUser.copy( id =1,  name ="Luis")
-    println(bro)
+//    println(bro)
 
     val friend = bro.copy( id =1,  name ="Roberto", lastName = "Ruiz", group = Group.FRIENDS)
-    println(friend)
+//    println(friend)
+
+    newTopic("Funciones de Alcance")
+    with(smartphone){
+        println("isPrivate => $isPrivate")
+    }
+
+        friend.group = Group.WORK
+        friend.name = "Mario"
+        friend.lastName = "Tellez"
+        println(friend)
+
+        friend.apply {
+            group = Group.FRIENDS
+            lastName = "Diaz"
+        }
+        println(friend)
 
 }
--- app/src/main/java/com/example/kotlinfundamentals/User.kt
+++ app/src/main/java/com/example/kotlinfundamentals/User.kt
@@ -1,7 +1,8 @@
 package com.example.kotlinfundamentals
 
 enum class Group{FAMILY, WORK, FRIENDS }
-
+// val es constante
+// var se puede cambiar
 data class User(val id: Long, var name: String, var lastName: String, var group: Group, var group2: Int) {
 
 }
